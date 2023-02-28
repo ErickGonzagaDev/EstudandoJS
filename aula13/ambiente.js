204d33b7b1bf3d1e44c3895a6cbc39acc0c618e4
ErickGonzagaDev <erickgonzaga.dev@gmail.com>
2023-02-28 05:40:14 -0300

    new file:   aula13/ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+/*
+
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+
+
+for (i = 0; i < 6; i++) {
+    console.log('Tudo bem?')
+}
+
+
+var i = 0
+do{
+    console.log('Tudo bem?')
+i++
+}while(i<6)
+
+*/
