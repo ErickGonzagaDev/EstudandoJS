649870aca4af3185664ed7562cf9bb3ab51bef57
ErickGonzagaDev <erickgonzaga.dev@gmail.com>
2023-02-28 06:58:52 -0300

    deleted:    aula12ex/imagens-c/imagens-c/background001.jpg 	deleted:    aula12ex/imagens-c/imagens-c/background002.jpg 	renamed:    aula12ex/imagens-c/background001.jpg -> aula12ex/imagens/background001.jpg 	renamed:    aula12ex/imagens-c/background002.jpg -> aula12ex/imagens/background002.jpg 	modified:   aula12ex/script.js

--- aula12ex/script.js
+++ aula12ex/script.js
@@ -9,16 +9,16 @@ function carregar() {
 
     if (hora >= 0 && hora < 12) {
         //Dia
-        img.src = '/aula12ex/imagens-c/background001.jpg'
+        img.src = '/aula12ex/imagens/background001.jpg'
         document.body.style.backgroundColor = 'yellow'
 
     } else if (hora >= 12 && hora < 18) {
         //Tarde
-        img.src = '/aula12ex/imagens-c/background002.jpg'
+        img.src = '/aula12ex/imagens/background002.jpg'
         document.body.style.backgroundColor = 'orange'
     } else {
         //Noite
-        img.src = '/aula12ex/imagens-c/background001.jpg'
+        img.src = '/aula12ex/imagens/background001.jpg'
         document.body.style.backgroundColor = 'brown'
 
     }
